2a2dbb73504019ad5c9cfaf7e90a830d7b40f32c
Nahuel Fedyszyn <32500578+NahuelFedyszyn@users.noreply.github.com>
2021-03-22 13:42:18 -0300

    22-3 Data now requested from API instead of local

--- src/views/Search/index.js
+++ src/views/Search/index.js
@@ -1,14 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import SearchBox from "./Components/SearchBox";
+import axios from "axios";
 import SearchResults from "./Components/SearchResults"
 import "./styles.css"
 import data from "../../data/users.json"
 
 export default function Search(){
     const [isAtTop,setIsAtTop] = useState(false);
-    const[userData,setUserData] = useState(data);
+    const[userData,setUserData] = useState([]);
     const [results, setResults] = useState([]);
+    
+    //Aqui se produce una llamada a la API
+    useEffect(()=>{
+        try{
+            const getUsers = async () =>{
+                const response = await axios.get("https://jsonplaceholder.typicode.com/users");
+                console.log(response);
+                //const response = await fetch("https://jsonplaceholder.typicode.com/users");
+                //const data = await response.json();
+                setUserData(response.data);
+            }
+            getUsers().catch(null)
+        }
+        catch(err){
+            console.error(err);
+        }
+    },[]);
 
+    //Hasta aqui la llamada a la API
 
     const handleSearchClick = (searchText) => {
         const searchTextMinus = searchText.toLowerCase();
